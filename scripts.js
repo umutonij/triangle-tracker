d5de8999f8bc225a03a776305956e7d3d1f2e7c0
jacquelineumutoni13@gmail.com <jacquelineumutoni13@gmail.com>
2018-12-14 16:22:25 +0200

    add functions

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function submit(){
+    var side1=parseFloat(document.getElementById("side1").value);
+    var side2=parseFloat(document.getElementById("side2").value);
+    var side3=parseFloat(document.getElementById("side3").value);
+
+    if(side1 ===side2 && side2 ===side3){
+        alert("This is an Equilateral triangle");
+    }
+    else if(side1 === side2 || side1 === side3 || side2 === side3){
+        alert("This is an Isosceles Triangle");
+    }
+    
+}
