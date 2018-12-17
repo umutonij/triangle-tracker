1573a3c25d1a2a688d0ec39b5e7a2c742ef1a76f
jacquelineumutoni13@gmail.com <jacquelineumutoni13@gmail.com>
2018-12-17 09:47:46 +0200

    add links that links javascript and htmls of equilateral, isosceles and scalene triangle

--- scripts.js
+++ scripts.js
@@ -5,14 +5,17 @@ function submit(){
 
     if(side1 ===side2 && side2 ===side3){
         alert("This is an Equilateral triangle");
+        window.location.href="eqltrl.html";
     }
     else if(side1 === side2 || side1 === side3 || side2 === side3){
         alert("This is an Isosceles Triangle");
+        window.location.href="isosceles.html";
     }
     else if(side1+side2) <= side3 || (side1 + side3) <= side2 || (side + side3) <= side1{
         alert("Not a definite TRiangle");
     }
     else {
-        alert("This is a Scalene Triangle")
+        alert("This is a Scalene Triangle");
+        window.location.href="scalene.html";
     }
 }
