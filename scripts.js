79edf61addc979a942ab6a8d557d971992195681
jacquelineumutoni13@gmail.com <jacquelineumutoni13@gmail.com>
2018-12-17 10:03:27 +0200

    make changes on javascripts and index

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-function submit(){
-    var side1=parseFloat(document.getElementById("side1").value);
-    var side2=parseFloat(document.getElementById("side2").value);
-    var side3=parseFloat(document.getElementById("side3").value);
+function getResult(){
+    var side1=parseFloat(document.getElementById('side1').value);
+    var side2=parseFloat(document.getElementById('side2').value);
+    var side3=parseFloat(document.getElementById('side3').value);
 
     if(side1 ===side2 && side2 ===side3){
         alert("This is an Equilateral triangle");
@@ -12,7 +12,7 @@ function submit(){
         window.location.href="isosceles.html";
     }
     else if(side1+side2) <= side3 || (side1 + side3) <= side2 || (side + side3) <= side1{
-        alert("Not a definite TRiangle");
+        alert("Not a definite Triangle");
     }
     else {
         alert("This is a Scalene Triangle");
