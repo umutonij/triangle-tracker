791b7dd9f365f9e9b9bff5a89eea826bb5c6113c
jacquelineumutoni13@gmail.com <jacquelineumutoni13@gmail.com>
2018-12-17 16:47:54 +0200

    make changes on javascript

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
+var sidesOfTriangle=["side1", "side2", "side3"];
 function getResult(){
-    var side1=parseFloat(document.getElementById('side1').value);
-    var side2=parseFloat(document.getElementById('side2').value);
-    var side3=parseFloat(document.getElementById('side3').value);
+    var side1 = parseFloat(document.getElementById('side1').value);
+    var side2 = parseFloat(document.getElementById('side2').value);
+    var side3 = parseFloat(document.getElementById('side3').value);
 
-    if(side1 ===side2 && side2 ===side3){
+    if(side1 === side2 && side2 === side3){
         alert("This is an Equilateral triangle");
         window.location.href="eqltrl.html";
     }
@@ -11,7 +12,7 @@ function getResult(){
         alert("This is an Isosceles Triangle");
         window.location.href="isosceles.html";
     }
-    else if(side1+side2) <= side3 || (side1 + side3) <= side2 || (side + side3) <= side1{
+    else if((side1+side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1){
         alert("Not a definite Triangle");
     }
     else {
